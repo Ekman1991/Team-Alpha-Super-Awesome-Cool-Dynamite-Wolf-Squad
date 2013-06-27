1ced2fab431c5bbff85523b389cd6cd43ee62c27
Philip Ekman <ekman1991@gmail.com>
2013-06-27 14:41:09 +0200

    Updated the tile class

--- TASACDWS/src/model/Tile.java
+++ TASACDWS/src/model/Tile.java
@@ -3,13 +3,23 @@ package model;
 import java.util.ArrayList;
 
 public class Tile {
-	private ArrayList<Tile> graphicObjects;
+	private ArrayList<IMovableGraphicObject> graphicObjects;
 	
 	
 	public Tile() {
-		
+		graphicObjects = new ArrayList<IMovableGraphicObject>();
 	}
 	
+	public void addObjectToTile (IMovableGraphicObject obj) {
+		graphicObjects.add(obj);
+	}
 	
+	public void removeObjectFromTile (IMovableGraphicObject obj) {
+		graphicObjects.remove(obj);
+	}
+	
+	public ArrayList<IMovableGraphicObject> getObjectsInTile () {
+		return graphicObjects;
+	}
 	
 }
