3aff3fd19b24c4ca76386436142f50127d84fa4f
bellevik <bellevik@gmail.com>
2013-06-27 14:41:06 +0200

    Updated World

--- TASACDWS/src/model/World.java
+++ TASACDWS/src/model/World.java
@@ -3,20 +3,31 @@ package model;
 public class World {
 	private static World instance;
 	private Tile[][] worldMatrix;
-	
+
 	private World () {
-		double worldWidth = Settings.getSettings().getWindowWidth()/Settings.tileSize;
-		double worldHeight = Settings.getSettings().getWindowHeight()/Settings.tileSize;
-		
-		
-		worldMatrix = new Tile[0][0];
+		double tempWidth = Settings.getSettings().getWindowWidth()/Settings.tileSize;
+		double tempHeight = Settings.getSettings().getWindowHeight()/Settings.tileSize;
+		// if ( 2 % 2 == 0)
+
+		int worldWidth = (int)tempWidth;
+		int worldHeight = (int)tempHeight;
+
+		if(((int)(tempWidth*2))%2 != 0) {
+			worldWidth += 1;
+		}
+
+		if(((int)(tempHeight*2))%2 != 0) {
+			worldHeight += 1;
+		} 
+
+		worldMatrix = new Tile[worldWidth][worldHeight];
 	}
-	
+
 	public synchronized static World getWorld() {
 		if(instance == null) {
 			instance = new World();
 		}
 		return instance;
 	}
-	
+
 }
