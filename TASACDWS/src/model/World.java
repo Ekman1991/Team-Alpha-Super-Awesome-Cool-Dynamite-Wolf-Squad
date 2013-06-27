11f598dd2eb69082a289c1d28ddc9cbc5025840a
Philip Ekman <ekman1991@gmail.com>
2013-06-27 14:10:03 +0200

    Updated world

--- TASACDWS/src/model/World.java
+++ TASACDWS/src/model/World.java
@@ -2,9 +2,10 @@ package model;
 
 public class World {
 	private static World instance;
+	private Tile[][] worldMatrix;
 	
 	private World () {
-		
+		worldMatrix = new Tile[12][12];
 	}
 	
 	public synchronized static World getWorld() {
